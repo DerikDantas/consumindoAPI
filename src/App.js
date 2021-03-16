07d3b2b86ab8463546dc6d13cf9515bc0412f5d8
Derik Oliveira <derikdantas@hotmail.com>
2021-03-16 14:20:00 -0300

    Versão 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.css'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      photos: [],
+      isLoading: false,
+      isError: false
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/photos")
+
+    if (response.ok) {
+      const photos = await response.json()
+      this.setState({ photos, isLoading: false })
+    } else {
+      this.setState({ isError: true, isLoading: false })
+    }
+  }
+
+
+  renderPhotos = () => {
+    return this.state.photos.map(photos => {
+      return (
+        <div key={photos.id} className="card">
+          <img src={photos.url} alt="..." />
+          <div className="title">
+            <h1>{photos.title}</h1>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    const { photos, isLoading, isError } = this.state
+
+    if (isLoading) {
+      return <div>Carregando...</div>
+    }
+
+    if (isError) {
+      return <div>Error...</div>
+    }
+
+    return photos.length > 0
+      ? (
+        <div class="container-card">{this.renderPhotos()}</div>
+      ) :
+      (
+        <div>No users</div>
+      )
+
+  }
 }
 
 export default App;
